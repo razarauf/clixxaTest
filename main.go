a462922b52c816721f729f6ccf6749f5bc44f968
razarauf <m.razarauf@gmail.com>
2017-03-22 22:00:53 -0700

    semi-fix avgtimes feature

--- main.go
+++ main.go
@@ -9,32 +9,52 @@ import (
 )
 
 func main() {
+	// check # of inputted arguments and which argument
 	if len(os.Args) < 2 || os.Args[1] != "avgtimes" {
 		panic("please run with arguments `avgtimes <filename>`")
 	}
 
+
 	data := make([]map[string]string, 0)
+	// open the file
 	if f, err := os.Open(os.Args[2]); err != nil {
 		panic(err.Error())
 	} else {
+	// decode file as JSON into the data variable
 		if err := json.NewDecoder(f).Decode(&data); err != nil {
 			panic(err.Error())
 		}
 	}
 
+
 	counts := make(map[string][]int, 0)
+
 	for _, k := range data {
-		timing, err := strconv.ParseInt(strings.TrimSuffix(k["timing"], "ms"), 10, 64)
-		if err != nil {
-			panic(err.Error())
+
+		if strings.Contains(k["timing"], "ms") {
+			timing, _ := strconv.ParseInt(strings.TrimSuffix(k["timing"], "ms"), 10, 64)
+			counts[k["category"]] = append(counts[k["category"]], int(timing))
+		} else if strings.Contains(k["timing"], "s"){
+			timing, _ := strconv.ParseInt(strings.TrimSuffix(k["timing"], "s"), 10, 64)
+			counts[k["category"]] = append(counts[k["category"]], int(timing) * 1e3)
 		}
-		counts[k["category"]] = append(counts[k["category"]], int(timing))
+		
+		// if err != nil {
+		// 	panic(err.Error())
+		// }
+
+		// counts[k["category"]] = append(counts[k["category"]], int(timing))
+		// fmt.Println(k);
 	}
+	
+	// fmt.Printf(counts)
+
 	for cat, times := range counts {
 		s := 0.0
 		for _, t := range times {
 			s += float64(t)
+			// fmt.Println(cat,"--", t)
 		}
-		fmt.Printf("cat %s\tavg %fms\n", cat, s/float64(len(times)))
+		fmt.Printf("category: %s\tavg %fms\n", cat, s/float64(len(times)))
 	}
 }
